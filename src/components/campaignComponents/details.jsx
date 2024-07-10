9da5ace6198d168f50b3b0b23278089853f34660
Passion-cyber <chineduibelachi@gmail.com>
2024-07-10 01:19:13 +0100

    Made changes to create campaign flow

--- src/components/campaignComponents/details.jsx
+++ src/components/campaignComponents/details.jsx
@@ -4,6 +4,7 @@ import campaignBanner from "@/assets/campaignBanner.svg";
 import Image from "next/image";
 import UploadIcon from "../../assets/uploadIcon.svg";
 import { Formik, Form, Field, ErrorMessage } from "formik";
+import { Button } from "@/components";
 
 const Details = () => {
   const [selectedImage, setSelectedImage] = useState(null);
@@ -54,7 +55,7 @@ const Details = () => {
           backgroundRepeat: "no-repeat",
         }}
       >
-        <h2 className="font-medium text-lg mb-2 text-[#FBFBFE]">
+        <h2 className="font-semibold text-lg mb-2 text-[#FBFBFE]">
           General Information
         </h2>
         <p className="font-normal text-sm text-[#FBFBFE]">
@@ -66,9 +67,7 @@ const Details = () => {
         {({ values, setFieldValue }) => (
           <Form className="flex flex-col gap-8">
             <div>
-              <p className="font-medium text-[#303036] text-[20px] mb-5">
-                Campaign Title
-              </p>
+              <p className="font-medium text-[#303036] mb-3">Campaign Title</p>
               <Field
                 className="border outline-none bg-transparent text-[#484851] font-normal text-[14px] rounded-lg w-full px-5 py-3 border-[#60606C]"
                 name="title"
@@ -77,9 +76,7 @@ const Details = () => {
             </div>
 
             <div className="">
-              <p className="font-medium text-[#303036] text-[20px] mb-5">
-                Display Banner
-              </p>
+              <p className="font-medium text-[#303036] mb-3">Display Banner</p>
 
               <div className=" rounded-lg border border-primary border-dashed bg-[#E7E7F9]">
                 {selectedImage ? (
@@ -122,8 +119,73 @@ const Details = () => {
             </div>
 
             <section>
-              <p className="font-bold text-[24px] text-[#303036]">Participants Specification</p>
+              <p className="font-bold text-[18px] text-[#303036] my-3">
+                Participants Specification
+              </p>
+              <div className="flex flex-col items-start gap-3 my-5">
+                <p className="font-medium text-[#303036]">
+                  Participants Status
+                </p>
+                <select
+                  className="border outline-none bg-transparent font-normal text-[14px] rounded-lg w-full px-5 py-3 border-[#0D0E32]"
+                  // value={values.proofOfPurchase.collectionId}
+                  // onChange={(event) => handleNFTChange(event, setFieldValue)}
+                >
+                  <option value="">Anyone can participate</option>
+                  <option value="">Family</option>
+                  <option value="">Friends</option>
+                </select>
+                <Field
+                  className="border-b border-b-[#60606C] outline-none bg-transparent text-[#484851] font-normal text-[14px] w-full px-5 py-3 "
+                  name="title"
+                  placeholder="Others, please specify"
+                />
+              </div>
+
+              <div className="space-y-3 my-5">
+                <p className="font-medium text-[#303036]">Participant Level</p>
+                <select
+                  className="border outline-none bg-transparent font-normal text-[14px] rounded-lg w-full px-5 py-3 border-[#0D0E32]"
+                  // value={values.proofOfPurchase.collectionId}
+                  // onChange={(event) => handleNFTChange(event, setFieldValue)}
+                >
+                  <option value="">Beginner</option>
+                  <option value="">Professional </option>
+                  <option value="">Master</option>
+                </select>
+              </div>
+              <div className="space-y-3 my-5">
+                <p className="font-medium text-[#303036]">Nationality</p>
+                <select
+                  className="border outline-none bg-transparent font-normal text-[14px] rounded-lg w-full px-5 py-3 border-[#0D0E32]"
+                  // value={values.proofOfPurchase.collectionId}
+                  // onChange={(event) => handleNFTChange(event, setFieldValue)}
+                >
+                  <option value="">Nigerian</option>
+                  <option value="">South African </option>
+                  <option value="">Zambian</option>
+                </select>
+              </div>
+              <div className="space-y-3 my-5">
+                <p className="font-medium text-[#303036]">Age Range</p>
+                <select
+                  className="border outline-none bg-transparent font-normal text-[14px] rounded-lg w-full px-5 py-3 border-[#0D0E32]"
+                  // value={values.proofOfPurchase.collectionId}
+                  // onChange={(event) => handleNFTChange(event, setFieldValue)}
+                >
+                  <option value="">20 - 25</option>
+                  <option value="">26 - 30 </option>
+                  <option value="">31 - 35</option>
+                </select>
+              </div>
             </section>
+
+            <div className="w-full">
+              <Button
+                name="Continue"
+                href={"/dashboard/campaign/create_campaign?route=action"}
+              />
+            </div>
           </Form>
         )}
       </Formik>
